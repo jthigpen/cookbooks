ae892aa24fff49e27bb31c588a19b7f2069fb0c8
James Thigpen <james.r.thigpen@gmail.com>
2013-09-15 18:39:48 -0700

    Setup db config for php hello world.

---
+++ helloworld/recipes/default.rb
@@ -0,0 +1,20 @@
+node[:deploy].each do |app_name, deploy|
+
+  template "#{deploy[:deploy_to]}/current/config.php" do
+    source "config.php.erb"
+    mode 0660
+    group "www-data"
+    owner "www-data"
+
+    variables(
+      :host =>     (deploy[:database][:host] rescue nil),
+      :user =>     (deploy[:database][:username] rescue nil),
+      :password => (deploy[:database][:password] rescue nil),
+      :db =>       (deploy[:database][:database] rescue nil),
+    )
+
+   only_if do
+     File.directory?("#{deploy[:deploy_to]}/current")
+   end
+  end
+end
